f76349f95548030a7a45f9136d7d5f56dae54074
cristian449 <144561600+cristian449@users.noreply.github.com>
2025-05-27 17:03:14 +0300

    This is a commit for Part-1

---
+++ NetflixClone/NetflixClone/MauiProgram.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+using NetflixClone.Pages;
+using NetflixClone.Services;
+
+namespace NetflixClone
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            builder.Services.AddHttpClient(TmdbService.TmdbHttpClientName,
+                httpClient => httpClient.BaseAddress = new Uri("https://api.themoviedb.org"));
+
+            builder.Services.AddSingleton<TmdbService>();
+            builder.Services.AddSingleton<MainPage>();
+
+            return builder.Build();
+        }
+    }
+}
